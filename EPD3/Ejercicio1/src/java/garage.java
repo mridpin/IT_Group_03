4afcbc24e80bce7f9204511fd34e7f74ce4712ee
victor-Martinelli <vicbomlen@gmail.com>
2018-02-20 14:14:34 +0100

    WIP EJ1

---
+++ EPD3/Ejercicio1/src/java/garage.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class garage {
+    
+    
+    public static List<parkingSpot> currentSports()
+    {
+        List<parkingSpot> list = new ArrayList<parkingSpot>();
+        
+        
+        return list;
+    }
+    
+}
